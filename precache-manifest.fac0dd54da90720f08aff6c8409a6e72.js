d6f4b09aabe2c929efa6091fa0bec39c5b0b7384
Anya <anya.p.nguyen@gmail.com>
2020-03-31 20:12:01 -0700

    Updates

---
+++ precache-manifest.fac0dd54da90720f08aff6c8409a6e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676e73361ff024bf6c7b19381a12c818",
+    "url": "/corona-virus-stats/index.html"
+  },
+  {
+    "revision": "d5cbe115b674583953f1",
+    "url": "/corona-virus-stats/static/css/main.1ac1542a.chunk.css"
+  },
+  {
+    "revision": "24da36348fade18eb0c2",
+    "url": "/corona-virus-stats/static/js/2.86137f46.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/corona-virus-stats/static/js/2.86137f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cbe115b674583953f1",
+    "url": "/corona-virus-stats/static/js/main.c7de4ad0.chunk.js"
+  },
+  {
+    "revision": "cec3b3ef616957d8b797",
+    "url": "/corona-virus-stats/static/js/runtime-main.bb688ff1.js"
+  },
+  {
+    "revision": "7215a9bdfac69f0d0a71cdb53d315bf0",
+    "url": "/corona-virus-stats/static/media/github.7215a9bd.svg"
+  }
+]);
